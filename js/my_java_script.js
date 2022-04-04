c7ca466dbc60b482ba023dcbae37ac7882749a02
MohamadAlSamour <99263535+MohamadAlSamour@users.noreply.github.com>
2022-04-04 15:44:25 +0200

    f

---
+++ js/my_java_script.js
@@ -0,0 +1,38 @@
+
+
+ function loaded(){
+    document.getElementById("arcade_video").style.display = ("none");
+    document.getElementById("website").style.display = ("none");
+    document.getElementById("info").style.display = ("");
+    document.getElementById("info_button").style.boxShadow = ("none");
+    document.getElementById("info_button").style.display = ("none");
+
+ }
+
+function Website()
+{
+    document.getElementById("website").style.display = ("");
+    document.getElementById("website").style.marginTop = ("5%");
+    document.getElementById("info").style.display = ("none");
+    document.getElementById("info_button").style.boxShadow = ("");
+    document.getElementById("info_button").style.display = ("");
+}
+function See_Info(){
+    document.getElementById("website").style.display = ("none");
+    document.getElementById("info").style.display = ("");
+    document.getElementById("info_button").style.boxShadow = ("");
+    document.getElementById("info_button").style.display = ("");
+}
+function Show_Arcade(){
+    document.getElementById("arcade_video").style.display = ("");
+    document.getElementById("arcade_video").style.marginTop = ("5%");
+    document.getElementById("info").style.display = ("none");
+    document.getElementById("info_button").style.boxShadow = ("");
+    document.getElementById("info_button").style.display = ("");
+}
+function See_Info_Arcade(){
+    document.getElementById("arcade_video").style.display = ("none");
+    document.getElementById("info").style.display = ("");
+    document.getElementById("info_button").style.boxShadow = ("");
+    document.getElementById("info_button").style.display = ("");
+}

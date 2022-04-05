bafa7726ce798427a2782d143769159590e5ff7b
MohamadAlSamour <99263535+MohamadAlSamour@users.noreply.github.com>
2022-04-05 03:05:54 +0200

    f

--- js/my_java_script.js
+++ js/my_java_script.js
@@ -1,20 +1,49 @@
 
-
  function loaded_Website(){
     document.getElementById("website").style.display = ("none");
     document.getElementById("info").style.display = ("");
     document.getElementById("info_button").style.boxShadow = ("none");
     document.getElementById("info_button").style.display = ("none");
 
- }
+}
 
- function loaded_Arcade(){
+function loaded_Arcade(){
     document.getElementById("arcade_video").style.display = ("none");
     document.getElementById("info").style.display = ("");
     document.getElementById("info_button").style.boxShadow = ("none");
     document.getElementById("info_button").style.display = ("none");
 
- }
+}
+function loaded_Data_visu(){
+    document.getElementById("visu_video").style.display = ("none");
+    document.getElementById("info").style.display = ("");
+    document.getElementById("info_button").style.boxShadow = ("none");
+    document.getElementById("info_button").style.display = ("none");
+}
+function loaded_Platformer_2D(){
+    document.getElementById("game_video").style.display = ("none");
+    document.getElementById("info").style.display = ("");
+    document.getElementById("info_button").style.boxShadow = ("none");
+    document.getElementById("info_button").style.display = ("none");
+}
+function loaded_Todo_App(){
+    document.getElementById("Todo_App").style.display = ("none");
+    document.getElementById("info").style.display = ("");
+    document.getElementById("info_button").style.boxShadow = ("none");
+    document.getElementById("info_button").style.display = ("none");
+}
+function loaded_Pong(){
+    document.getElementById("Pong_video").style.display = ("none");
+    document.getElementById("info").style.display = ("");
+    document.getElementById("info_button").style.boxShadow = ("none");
+    document.getElementById("info_button").style.display = ("none");
+}
+function loaded_Piano(){
+    document.getElementById("Piano_video").style.display = ("none");
+    document.getElementById("info").style.display = ("");
+    document.getElementById("info_button").style.boxShadow = ("none");
+    document.getElementById("info_button").style.display = ("none");
+}
 
 function Website()
 {
@@ -39,6 +68,66 @@ function Show_Arcade(){
 }
 function See_Info_Arcade(){
     document.getElementById("arcade_video").style.display = ("none");
+    document.getElementById("info").style.display = ("");
+    document.getElementById("info_button").style.boxShadow = ("");
+    document.getElementById("info_button").style.display = ("");
+}
+function Show_visu(){
+    document.getElementById("visu_video").style.display = ("");
+    document.getElementById("info").style.display = ("none");
+    document.getElementById("info_button").style.boxShadow = ("");
+    document.getElementById("info_button").style.display = ("");
+}
+function See_Info_Visu(){
+    document.getElementById("visu_video").style.display = ("none");
+    document.getElementById("info").style.display = ("");
+    document.getElementById("info_button").style.boxShadow = ("");
+    document.getElementById("info_button").style.display = ("");
+}
+function Show_Platformer_2D(){
+    document.getElementById("game_video").style.display = ("");
+    document.getElementById("info").style.display = ("none");
+    document.getElementById("info_button").style.boxShadow = ("");
+    document.getElementById("info_button").style.display = ("");
+}
+function See_Info_Platformer_2D(){
+    document.getElementById("game_video").style.display = ("none");
+    document.getElementById("info").style.display = ("");
+    document.getElementById("info_button").style.boxShadow = ("");
+    document.getElementById("info_button").style.display = ("");
+}
+function Show_Todo_App(){
+    document.getElementById("Todo_App").style.display = ("");
+    document.getElementById("info").style.display = ("none");
+    document.getElementById("info_button").style.boxShadow = ("");
+    document.getElementById("info_button").style.display = ("");
+}
+function See_Info_Todo_App(){
+    document.getElementById("Todo_App").style.display = ("none");
+    document.getElementById("info").style.display = ("");
+    document.getElementById("info_button").style.boxShadow = ("");
+    document.getElementById("info_button").style.display = ("");
+}
+function Show_Pong(){
+    document.getElementById("Pong_video").style.display = ("");
+    document.getElementById("info").style.display = ("none");
+    document.getElementById("info_button").style.boxShadow = ("");
+    document.getElementById("info_button").style.display = ("");
+}
+function See_Info_Pong(){
+    document.getElementById("Pong_video").style.display = ("none");
+    document.getElementById("info").style.display = ("");
+    document.getElementById("info_button").style.boxShadow = ("");
+    document.getElementById("info_button").style.display = ("");
+}
+function Show_Piano(){
+    document.getElementById("Piano_video").style.display = ("");
+    document.getElementById("info").style.display = ("none");
+    document.getElementById("info_button").style.boxShadow = ("");
+    document.getElementById("info_button").style.display = ("");
+}
+function See_Info_Piano(){
+    document.getElementById("Piano_video").style.display = ("none");
     document.getElementById("info").style.display = ("");
     document.getElementById("info_button").style.boxShadow = ("");
     document.getElementById("info_button").style.display = ("");
